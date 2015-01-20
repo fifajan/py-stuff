8efc2e548195097d37e3f054ccd64e7bda764ceb
Oleksiy Ivchenko <ol.ivchenko@samsung.com>
2015-01-20 16:25:38 +0200

    Add tutorial link

--- data_structures/red_black_tree/red_black_tree.py
+++ data_structures/red_black_tree/red_black_tree.py
@@ -1,7 +1,12 @@
-#! /ur/bin/python
+#! /usr/bin/python
 
 class RBTree(object):
     '''
     My attempt to implement Red Black Tree.
+    Implementation follows this great tutorial:
+    http://www.eternallyconfuzzled.com/tuts/datastructures/jsw_tut_rbtree.aspx
     '''
+    pass
+
+class RBNode(object):
     pass
