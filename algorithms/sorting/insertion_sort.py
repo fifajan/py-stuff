4669e66907f1373ebf2c3aac064b7f8de06ab330
Oleksiy Ivchenko <ol.ivchenko@samsung.com>
2015-01-20 13:43:06 +0200

    Add insertion sort implementation

---
+++ algorithms/sorting/insertion_sort.py
@@ -0,0 +1,28 @@
+#! /usr/bin/python
+
+# to test it run:
+# $ ./insertion_sort.py array.txt
+# it will take a LONG time (because of O(n^2))
+
+from sys import argv
+
+def sort(array):
+    '''
+    Insertion Sort implementation.
+    '''
+    array_len_r = range(1, len(array))
+    for i in array_len_r:
+        prev_i = i - 1
+        current = array[i]
+        while prev_i >= 0 and array[prev_i] > current:
+            array[prev_i + 1] = array[prev_i]
+            array[prev_i] = current
+            prev_i -= 1
+    return array
+
+array1 = [int(line) for line in file(argv[1])]
+array2 = array1[::]
+
+print '- Does this algorithm work correctly? (checking it now...)'
+print '- ' + 'Yes!' if sort(array1) == sorted(array2) else 'Nope!'
+print array
