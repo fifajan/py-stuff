35e8e06b392ff16679ca96daafbdc9109083a59f
Oleksiy Ivchenko <ol.ivchenko@samsung.com>
2015-01-20 17:17:59 +0200

    Implement RB assert

--- data_structures/red_black_tree/red_black_tree.py
+++ data_structures/red_black_tree/red_black_tree.py
@@ -24,9 +24,42 @@ class RBTree(object):
         return save
 
     def rot_2(self, root, dir)
+        '''
+        Double rotation
+        '''
         root.nodes[not dir] = self.rot_1(root.nodes[not dir], not dir)
         return self.rot_1(root, dir)
 
+    def rb_assert(root):
+        '''
+        Print message in case there are some Red/Black violations
+        '''
+        lh, rh = 0
+
+        if not root:
+            return True
+        else:
+            ln, rn = root.nodes
+
+            if root.is_red and (ln.is_red or rn.is_red):
+                print 'Red violation'
+                return False
+
+            lh = self.rb_assert(ln)
+            rh = self.rb_assert(rn)
+
+            if (ln and ln.data >= root.data) or (rn and rn.data <= root.data):
+                print 'Binary tree violation'
+                return False
+
+            if lh and rh and (lh != rh):
+                print 'Black violation'
+                return False
+
+            if lh and rh:
+                return lh if root.is_red else lh + 1
+            else:
+                return False
 
 class RBNode(object):
     '''
@@ -35,5 +68,5 @@ class RBNode(object):
     def __init__(self):
         self.is_red = False
         self.data = []
-        self.nodes = []
+        self.nodes = [] # 2 nodes: left ([0]) & right ([1])
 
