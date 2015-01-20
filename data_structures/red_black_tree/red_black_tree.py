5932d29360c48ac05944320560da46e005b6eeea
Oleksiy Ivchenko <ol.ivchenko@samsung.com>
2015-01-20 16:57:32 +0200

    In progress

--- data_structures/red_black_tree/red_black_tree.py
+++ data_structures/red_black_tree/red_black_tree.py
@@ -6,7 +6,27 @@ class RBTree(object):
     Implementation follows this great tutorial:
     http://www.eternallyconfuzzled.com/tuts/datastructures/jsw_tut_rbtree.aspx
     '''
-    root = None
+    def __init__(self):
+        root = None
+
+    def rot_1(self, root, dir):
+        '''
+        Single rotation
+        '''
+        save = root.nodes[not dir]
+
+        root.nodes[not dir] = save.nodes[dir]
+        save.nodes[dir] = root
+
+        root.is_red = True
+        save.is_red = False
+
+        return save
+
+    def rot_2(self, root, dir)
+        root.nodes[not dir] = self.rot_1(root.nodes[not dir], not dir)
+        return self.rot_1(root, dir)
+
 
 class RBNode(object):
     '''
