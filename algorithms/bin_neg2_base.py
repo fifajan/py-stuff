ec1aeb5efa042ccab23108e3c1fe146d67d764a2
Oleksiy Ivchenko <ol.ivchenko@samsung.com>
2015-02-03 09:23:15 +0200

    Add binary tools for (-2) base system.

---
+++ algorithms/bin_neg2_base.py
@@ -0,0 +1,23 @@
+#! /usr/bin/python
+
+'''
+Tools for (-2) base binary system.
+'''
+
+def get_number_neg2(bits):
+    return sum(b * ((-2)**i) for i, b in enumerate(bits))
+
+def get_bits_neg2(number):
+    result = []
+    while number:
+        remainder = number % -2
+        number /= -2
+        if remainder < 0:
+            remainder += 2
+            number += 1
+        result.append(remainder)
+    return result
+
+if __name__ == '__main__':
+    assert 5 == get_number_neg2([1, 0, 1])
+    assert [0, 1, 0, 0, 1] == get_bits_neg2(14)
