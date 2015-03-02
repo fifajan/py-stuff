84053fe5e0727726f7cebde7c68010a1f4a4ae8b
Oleksiy Ivchenko <ol.ivchenko@samsung.com>
2015-03-02 17:37:20 +0200

    Add template for selection sort implementation.

---
+++ algorithms/sorting/selection_sort.py
@@ -0,0 +1,16 @@
+#! /usr/bin/python
+
+# to test it run:
+# $ ./selection_sort.py array.txt
+
+# tests:
+#    Array state     Time     (array is 10^4 integers text file)
+# ---------------------------
+#         random    ? s
+#       inverted    ? s
+
+from sys import argv
+
+# print '- Does this algorithm work correctly? (checking it now...)'
+# print '- ' + 'Yes!' if sort(array1) == sorted(array2) else 'Nope!'
+
