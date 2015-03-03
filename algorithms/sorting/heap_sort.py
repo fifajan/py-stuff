c72fc832562d67e2d92588495e46ed583282566a
Oleksiy Ivchenko <ol.ivchenko@samsung.com>
2015-03-03 14:33:30 +0200

    Start Priority Queue (min heap) implementation.

--- algorithms/sorting/heap_sort.py
+++ algorithms/sorting/heap_sort.py
@@ -11,6 +11,35 @@
 
 from sys import argv
 
+class PriorityQueue(object):
+    """My attempt to implement a priority queue.
+    
+    http://www.cs.cmu.edu/~adamchik/15-121/lectures/Binary%20Heaps/heaps.html
+    resource was used for heap implementation details reference.
+    """
+
+    def __init__(self):
+        self.heap = []
+        self.size = 0
+        # defines if it will be max or min heap:
+        self.min_root = True
+
+    def double_size(self):
+        old_heap = self.heap
+        self.heap = [None] * (2 * self.size)
+        self.heap[1:self.size] = old_heap[1:]
+
+    def insert(self, value):
+        if self.size == len(self.heap - 1):
+            self.double_size()
+
+        self.size += 1
+        pos = self.size
+        while pos > 1 and XXXX: # TODO
+            self.heap[pos] = self.heap[pos // 2]
+
+        self.heap[pos] = value
+
 # print '- Does this algorithm work correctly? (checking it now...)'
 # print '- ' + 'Yes!' if sort(array1) == sorted(array2) else 'Nope!'
 
