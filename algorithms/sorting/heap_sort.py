d5aaca69f8b5753896610df19147e53df8eac342
Oleksiy Ivchenko <ol.ivchenko@samsung.com>
2015-03-05 11:38:06 +0200

    Finish & test heap implementation.

--- algorithms/sorting/heap_sort.py
+++ algorithms/sorting/heap_sort.py
@@ -19,10 +19,14 @@ class PriorityQueue(object):
     """
 
     def __init__(self):
-        self.heap = []
+        self.heap = [None, None]
         self.size = 0
         # defines if it will be max or min heap:
         self.min_root = True
+
+    def build_heap(self):
+        for k in reversed(range(1, (self.size // 2) + 1)):
+            self.percolate_down(k)
 
     def double_size(self):
         old_heap = self.heap
@@ -30,18 +34,32 @@ class PriorityQueue(object):
         self.heap[1:self.size] = old_heap[1:]
 
     def insert(self, value):
-        if self.size == len(self.heap - 1):
+        if self.size == len(self.heap) - 1:
             self.double_size()
 
         self.size += 1
         pos = self.size
-        while pos > 1 and heap[pos // 2] > value:
+        while pos > 1 and self.heap[pos // 2] > value:
             self.heap[pos] = self.heap[pos // 2]
+            pos = pos // 2
 
         self.heap[pos] = value
 
     def percolate_down(self, k):
-        pass # TODO: implement
+        tmp = self.heap[k]
+        child = 0
+
+        while 2 * k <= self.size:
+            child = 2 * k
+            child += 1 if child != self.size and (
+                            self.heap[child + 1] < self.heap[child]) else 0
+            if self.heap[child] < tmp:
+                self.heap[k] = self.heap[child]
+            else:
+                break
+            k = child
+
+        self.heap[k] = tmp
 
     def pop_min(self):
         if self.size == 0:
@@ -52,6 +70,15 @@ class PriorityQueue(object):
             self.size -= 1
             self.percolate_down(1)
             return min 
+
+    def __len__(self):
+        return self.size
+
+    def __str__(self):
+        return repr(self)
+
+    def __repr__(self):
+        return str(self.heap)
 
 # print '- Does this algorithm work correctly? (checking it now...)'
 # print '- ' + 'Yes!' if sort(array1) == sorted(array2) else 'Nope!'
