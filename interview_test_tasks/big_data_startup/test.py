c316ce9c1002e580ac786c429e1c745d6b188984
me <me@swdevelop.net>
2015-03-07 14:07:26 +0200

    Added alg complexity (time, space) info to comments.

--- interview_test_tasks/big_data_startup/test.py
+++ interview_test_tasks/big_data_startup/test.py
@@ -9,20 +9,24 @@ from gen_int_arrays import write_int_array_to_text_file
 
 class TestBigData(unittest.TestCase):
     """Here we are going to test our algorithm with 100 [k] streams (read 
-    from 100 files) with 2,000 integers in each.
+    from 100 files) with 4,000 [m] integers in each.
 
-    So total n is 2,000,000 here.
+    So total n is 4,000,000 here.
 
     With this input algorithm performs about (compared to my implemenations):
         3 times slower than mergesort;
       2.5 times slower than quicksort;
           about the same speed as heapsort;
       BUT requires only k memory (for the minimum heap).
+
+      SO:
+          time complexity is O(m*log(k));
+          space complexity is O(k);
     """
 
     name_pat = 'i%04i.txt'
     n_files = 100
-    n_ints_in_file = 20000
+    n_ints_in_file = 40000
 
     def setUp(self):
         # create test files:
