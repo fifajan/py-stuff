10007426f23b79b73ddbaee24ab0494cae2d601c
me <me@swdevelop.net>
2015-03-09 09:11:44 +0200

    Add deque-powered merge function and time test for it.

--- algorithms/sorting/merge_sort.py
+++ algorithms/sorting/merge_sort.py
@@ -10,6 +10,7 @@
 #       inverted    0.1 s
 
 from sys import argv
+from collections import deque
 
 def sort(arr):
     '''
@@ -21,16 +22,16 @@ def sort(arr):
     l, r = arr[:mid], arr[mid:]
     l, r = sort(l), sort(r)
     return merge(l, r)
- 
+
 def merge(l, r):
     '''
     This merge func works exactly as it should.
     '''
     res = []
     i = j = 0
-    ll = len(l)
-    lr = len(r)
-    while i < ll and j < lr:
+    l_l = len(l)
+    r_l = len(r)
+    while i < l_l and j < r_l:
         if l[i] < r[j]:
             res.append(l[i])
             i += 1
@@ -39,15 +40,29 @@ def merge(l, r):
             j += 1
     return res + l[i:] + r[j:]
 
-def merge_pretty(l, r):
+def merge_list_pop(l, r):
     '''
-    This merge func looks good but works slower than it should.
+    This merge func looks good but works significantly slower than it should.
+    10 time slower than merge on only 200K int sorting.
     '''
     res = []
     lr_min = lambda: l.pop(0) if l[0] < r[0] else r.pop(0)
     while l and r:
-        res += [lr_min()]
+        res.append(lr_min())
     return res + l + r
+
+
+def merge_deque_pop(l, r):
+    '''
+    Looks quite good too and uses deque but works a bit slower than it should.
+    1.5 time slower than merge on 2M int sorting.
+    '''
+    l_q, r_q = deque(l), deque(r)
+    res = []
+    lr_min = lambda: l_q.popleft() if l_q[0] < r_q[0] else r_q.popleft()
+    while l_q and r_q:
+        res.append(lr_min())
+    return res + list(l_q) + list(r_q)
 
 if __name__ == '__main__':
     array1 = [int(line) for line in file(argv[1])]
