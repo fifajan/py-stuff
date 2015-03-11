777dfd4f2f101618de431368b72c05fdbbf1d8f9
Oleksiy Ivchenko <ol.ivchenko@samsung.com>
2015-03-11 14:44:17 +0200

    Add text maze to 1/0 matrix maze conversion; Add cool test cases.

--- solvers/dijkstras_maze_solver.py
+++ solvers/dijkstras_maze_solver.py
@@ -10,18 +10,25 @@ class DijkstrasMazeSolver(object):
     """
     infinity = 10**10
 
-    def __init__(self, input_maze=None, is_graph=False):
-        if not is_graph:
+    def __init__(self, input_maze=None):
+        is_text = type(input_maze) == str
+        if is_text:
+            converter = TextToMatrixMazeConverter(input_maze)
+            converter = MazeToGraphConverter(converter.maze,
+                                             converter.start_cell)
+        else:
             converter = MazeToGraphConverter(input_maze)
-            self.vertices = converter.graph_vertices
-            self.edge_weights = converter.graph_edge_weights
-            self.adjacency_list = converter.graph_adjacency_list
-            self.start_v = converter.start_cell
-            self.exit_v = converter.exit_cell
-            self.dist = dict()
-            self.prev = dict()
-            self.queue = PriorityQueue()
-            self.shortest_path = self.shortest_exit_path()
+
+        self.vertices = converter.graph_vertices
+        self.edge_weights = converter.graph_edge_weights
+        self.adjacency_list = converter.graph_adjacency_list
+        self.edge_weights = converter.graph_edge_weights
+        self.start_v = converter.start_cell
+        self.exit_v = converter.exit_cell
+        self.dist = dict()
+        self.prev = dict()
+        self.queue = PriorityQueue()
+        self.shortest_path = self.shortest_exit_path()
 
     def shortest_exit_path(self):
         self.count_shortest_paths(self.start_v)
@@ -94,6 +101,7 @@ class MazeToGraphConverter(object):
         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],  
     ]                                            Nodes are:
                                                     - start cell;
+                                                    - exit cell;
                                                     - dead end cells;
                                                     - 3 and 4 way crossings.
 
@@ -101,9 +109,9 @@ class MazeToGraphConverter(object):
                                            Simplified weighted graph:
 
                                                        (D) 
-    Simplified maze (graph inside):                     #
-                                                        #
-    +----------------+-------------+                    #
+    Simplified maze (graph inside):    start vertex     #
+                                            |           #
+    +----------------+-------------+        V           #
     |(A)############ | ########### |       (A)###(B)###(C)
     |   +--------+ # + # +-----+ # |                    #
     |             (B)#(C)| ####### |                    # <-- each edge is
@@ -154,17 +162,19 @@ class MazeToGraphConverter(object):
             return False
 
     def possible_moves(self, x, y):
-        return ''.join([d for d in 'URLD' if self.can_go(x, y, d)])
+        return ''.join([d for d in 'DRUL' if self.can_go(x, y, d)])
 
     def make_vertices_and_edges(self, x, y, visited):
+        # print x, y
         if not self.exit_cell and self.maze[y][x] > 1:
             self.exit_cell = (x, y)
         possible_moves = self.possible_moves(x, y)
         if (len(visited) == 1 # start vertex
-                ) or len(possible_moves) in (
-                    1,  # dead end
-                    3,  # 3-way crossing
-                    4): # 4-way crossing
+                ) or (len(possible_moves) in (
+                    1, # dead end
+                    3, # 3-way crossing
+                    4) # 4-way crossing
+                        ) or (x, y) == self.exit_cell: # exit vertex
             self.graph_vertices.add((x, y))
             for i, (x1, y1) in enumerate(visited):
                 if (x, y) != (x1, y1) and (x1, y1) in self.graph_vertices:
@@ -195,7 +205,73 @@ class MazeToGraphConverter(object):
         self.make_adjacency_list()
 
 
+class TextToMatrixMazeConverter(object):
+    """Converts text-maze to matrix 0/1 maze.
+    Example:
+
+    XXXXX      [ [1, 1, 1, 1, 1],
+    X S X        [1, 0, 0, 0, 1],
+    X   X   ==>  [1, 0, 0, 0, 1],
+    X XXX        [1, 0, 1, 1, 1],
+    X E X        [1, 0, 2, 0, 1],
+    XXXXX        [1, 1, 1, 1, 1] ]
+    """
+    def __init__(self, maze):
+        self.maze_to_matrix(maze)
+
+    def maze_to_matrix(self, maze):
+        self.maze = []
+        self.start_cell = None
+        for li, line in enumerate(maze.split('\n')):
+            maze_line = []
+            for ci, char in enumerate(line):
+                if char.upper() == 'S':
+                    maze_line.append(0)
+                    self.start_cell = (ci, li)
+                elif char.upper() == 'E':
+                    maze_line.append(2)
+                elif char.isspace():
+                    maze_line.append(0)
+                else:
+                    maze_line.append(1)
+            self.maze.append(maze_line)
+
+
 if __name__ == '__main__':
+    # ATTENTION:
+    # running all those tests could require 5-15 seconds.
+    maze = ('######\n'
+            '# S  #\n'
+            '#### #\n'
+            '#    #\n'
+            '# ####\n'
+            '#    #\n'
+            '## ###\n'
+            '## ###\n'
+            '#   E#\n'
+            '######')
+
+    assert DijkstrasMazeSolver(maze).shortest_path == (
+                ((2, 1), (2, 5), (2, 8), (4, 8)), 15) # path, len(path)
+
+    # takes a bit long time due to rooms
+    maze = ('XXX+-+-----------------------+\n'
+            'XXX| |                       |\n'
+            'XXX| +===+ +=+ +=+ +-+-->    |\n'
+            '+--^ |S    | | | | | |       |\n'  # S - start
+            '|    +--+--+ | | | | | <-+===+\n'
+            '+-----+ |    | | | | |   \WWWW\n'
+            '|       |    +-+ +-+ V <-+===+\n'
+            '| <=====+=+                  |\n'
+            '|            _.-+ +--+ .._/^^~\n'
+            '+=====+->   /~~~|E|~~| |~~~~~~\n'  # E - exit
+            ' }O_o{|     |~~~+-+~~+~+~~~~~~\n'
+            '  ^^^ |_/\__/~~~~~~~~~~~~~~~~~\n')
+            
+    assert DijkstrasMazeSolver(maze).shortest_path == (
+        ((6, 3), (10, 1), (14, 1), (18, 1), (25, 1), (25, 2), (25, 3),
+         (22, 5), (22, 7), (20, 7), (17, 7), (17, 9)), 37)
+
     # example from MazeToGraphConverter's docstr:
     maze = [
         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -209,9 +285,8 @@ if __name__ == '__main__':
         [1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1],
         [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1],
         [1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
     ]
-
     assert DijkstrasMazeSolver(maze).shortest_path == (
             ((1, 1), (4, 4), (6, 4), (8, 7), (4, 7), (1, 6)), 31) # path, len
 
@@ -229,23 +304,33 @@ if __name__ == '__main__':
         [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 1],
         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
     ]
-
     assert DijkstrasMazeSolver(maze).shortest_path == (
             ((1, 1), (1, 6), (3, 6), (4, 7), (6, 7), (8, 8), (10, 10)), 18)
 
-    # TODO: handle this case ->
     # one room maze (infinite loop test)
-    # maze = [
-    #     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    #     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    #     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    #     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    #     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    #     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    #     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    #     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    #     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    #     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    #     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 1],
-    #     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-    # ]
+    maze = [
+        [1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 2, 1],
+        [1, 1, 1, 1, 1, 1],
+    ]
+    assert DijkstrasMazeSolver(maze).shortest_path == (
+        ((1, 1), (1, 2), (1, 3), (1, 4), (1, 5), (2, 6), (3, 6), (4, 6)), 8)
+
+    # one room maze with walls (infinite loop test)
+    maze = [
+        [1, 1, 1, 1, 1, 0, 1],
+        [1, 0, 0, 1, 0, 2, 1],
+        [1, 0, 0, 1, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 1],
+        [1, 0, 1, 0, 0, 0, 1],
+        [1, 0, 1, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1],
+    ]
+    assert DijkstrasMazeSolver(maze).shortest_path == (
+        ((1, 1), (1, 2), (1, 3), (2, 3), (3, 3), (4, 3), (4, 2), (5, 1)), 8)
