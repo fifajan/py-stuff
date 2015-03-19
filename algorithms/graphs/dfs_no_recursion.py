80e61ebbea8acc268a7076d6266bca51fd625916
me <me@swdevelop.net>
2015-03-19 12:57:59 +0200

    Implement path seacrh using non resursive DFS.

--- algorithms/graphs/dfs_no_recursion.py
+++ algorithms/graphs/dfs_no_recursion.py
@@ -1,1 +1,31 @@
+"""Non recursive approach to get path from one vertext to other
+using DFS (depth first search).
+Returns None if path does not exist in a given graph.
+"""
+
+stack = list # standard list should do well as a stack
+
+def find_path_dfs(adj_list, from_v, to_v):
+    path = []
+    vertices_stack = stack((from_v,))
+    while vertices_stack:
+        vertex = vertices_stack.pop()
+        path = fix_path(adj_list, path, vertex) + [vertex]
+        if vertex == to_v:
+            return path
+        vertices_stack.extend(adj_list[vertex])
+
+def fix_path(adj_list, path, vertex):
+    if path:
+        path_copy = path[:]
+        for v in reversed(path_copy):
+            if vertex not in adj_list[v]:
+                path.pop()
+            else:
+                break
+    return path
+            
+    
+    
+
 
