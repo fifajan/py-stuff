a1471c5e1ea481e852aeb622bb31be38a16b0628
me <me@swdevelop.net>
2015-03-19 13:22:39 +0200

    Implement BFS.

--- algorithms/graphs/bfs_no_recursion.py
+++ algorithms/graphs/bfs_no_recursion.py
@@ -1,1 +1,29 @@
+"""Non recursive approach to get path from one vertext to other
+using BFS (breadth first search).
+Returns None if path does not exist in a given graph.
+"""
 
+from collections import deque as queue
+
+def find_path_bfs(adj_list, from_v, to_v):
+    path = []
+    vertices_queue = queue((from_v,))
+    visited = set()
+    while vertices_queue:
+        vertex = vertices_queue.pop()
+        if vertex not in visited:
+            visited.add(vertex)
+            path = fix_path(adj_list, path, vertex) + [vertex]
+            if vertex == to_v:
+                return path
+            vertices_queue.extendleft(adj_list[vertex])
+
+def fix_path(adj_list, path, vertex):
+    if path:
+        path_copy = path[:]
+        for v in reversed(path_copy):
+            if vertex not in adj_list[v]:
+                path.pop()
+            else:
+                break
+    return path
