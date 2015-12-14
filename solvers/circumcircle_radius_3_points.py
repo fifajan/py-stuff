0dfe22a743f7acfd9ba74dfb9219fb5a17bd3203
me <me@swdevelop.net>
2015-12-14 16:29:43 +0200

    Add circumcircle radius for triangle finder.

---
+++ solvers/circumcircle_radius_3_points.py
@@ -0,0 +1,22 @@
+#! /usr/bin/python
+
+def distance(a, b):
+    """Gets distance between 2 given x,y points."""
+    ax, ay = a
+    bx, by = b
+    return ((ax - bx)**2 + (ay - by)**2)**.5
+
+def radius(a, b, c):
+    """Gets radius of circumscribed circle for abc triangle."""
+    ab = distance(a, b)
+    ac = distance(a, c)
+    bc = distance(b, c)
+
+    a, b, c = ab, ac, bc
+
+    return (a * b * c) / (
+                (a + b + c) * (-a + b + c) * (a - b + c) * (a + b - c))**.5
+
+
+if __name__ == '__main__':
+    assert abs(radius((0, 0), (1, 0), (0, 1)) - 0.707106) < .00001
