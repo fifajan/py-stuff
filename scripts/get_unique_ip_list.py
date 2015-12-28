f798ddd0d154a2db85517db359edea2de9d6e04c
me <me@swdevelop.net>
2015-12-28 15:10:40 +0000

    Enhanced unique IP script.

--- scripts/get_unique_ip_list.py
+++ scripts/get_unique_ip_list.py
@@ -1,19 +1,27 @@
 #! /usr/bin/python
 
-WEB_SERVER_ACCESS_LOG_FILE = 'xxx.log'
-OUTPUT_UNIQUE_IPS_LIST_FILE = 'IPs.log'
+if __name__ == '__main__':
 
-f = open(WEB_SERVER_ACCESS_LOG_FILE)
-l = f.readlines()
-ips = [i.partition(' ')[0] for i in l]
+    from sys import argv
 
-# get unique IPs
-s = set(ips)
-f.close()
+    if len(argv) != 2:
+        print '\nUSAGE: $ ./get_unique_ip_list.py /path/to/logfile.log\n'
+        raise RuntimeError, 'Bad input.'
 
-# write IPs to file
-f = open(OUTPUT_UNIQUE_IPS_LIST_FILE, 'w')
-for i in s:
-    f.write(str(i) + '\n')
+    WEB_SERVER_ACCESS_LOG_FILE = argv[1]
+    OUTPUT_UNIQUE_IPS_LIST_FILE = 'IPs.log'
 
-f.close()
+    f = open(WEB_SERVER_ACCESS_LOG_FILE)
+    l = f.readlines()
+    ips = [i.partition(' ')[0] for i in l]
+
+    # get unique IPs
+    s = set(ips)
+    f.close()
+
+    # write IPs to file
+    f = open(OUTPUT_UNIQUE_IPS_LIST_FILE, 'w')
+    for i in s:
+        f.write(str(i) + '\n')
+
+    f.close()
