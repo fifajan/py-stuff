07cf95fb81eba24773e20b8663035901adc9b5c4
fifajan <me@swdevelop.net>
2016-01-12 11:24:40 +0200

    Fix test.

--- data_structures/red_black_tree/test.py
+++ data_structures/red_black_tree/test.py
@@ -10,12 +10,14 @@ from red_black_tree import RBTree
 
 c_range = range(40, 121)
 rc = lambda : chr(choice(c_range))
+rand_4_chars = lambda: rc() + rc() + rc() + rc()
+
 print_t = lambda t : 'Tree:\n%s' % t
 
 print '#### Only insertions:'
 t = RBTree()
 for i in range(1000):
-    s = rc() + rc() + rc() + rc()
+    s = rand_4_chars()
     t.insert(s)
 
 print print_t(t) + '\n#### 4/4 insertions then 3/4 removal:'
@@ -24,7 +26,7 @@ print print_t(t) + '\n#### 4/4 insertions then 3/4 removal:'
 t = RBTree()
 values = set()
 for i in range(4000):
-    s = rc() + rc() + rc()
+    s = rand_4_chars()
     if i > 999:
         values.add(s)
     t.insert(s)
